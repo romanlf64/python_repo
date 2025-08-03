4de6527b1df56bc82ec64589e23f472be3f0c81f
romanlf64 <romanlf64@gmail.com>
2025-08-03 12:51:06 +0700

    add first homework

---
+++ lesson_01/lesson_01_1.py
@@ -0,0 +1,23 @@
+my_heigh = 172
+print(my_heigh)
+
+my_name = 'Leonid'
+my_name = 'Leonid Romanov'
+print(my_name)
+
+pet_name = input("как зовут вашего питомца? ")
+print("моя любимица - " + pet_name)
+
+def print_python():
+    print('I am learning Python')
+print_python()
+
+def print_letter(let):
+    print(let, end = '')
+print_letter('S')
+print_letter('t')
+print_letter('u')
+print_letter('d')
+print_letter('e')
+print_letter('n')
+print_letter('t')
---
+++ lesson_01/lesson_1_task_2.py
@@ -0,0 +1,3 @@
+my_age = 60
+my_age = 63
+print(my_age)
