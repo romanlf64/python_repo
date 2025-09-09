e451b685cd54dc88e12969b66b25a04f6695463d
romanlf64 <romanlf64@gmail.com>
2025-09-09 13:54:40 +0700

    add third homework

---
+++ lesson_03/mailing.py
@@ -0,0 +1,10 @@
+class Mailing:
+    def __init__(self, from_address, to_address, cost, track):
+        self.to_address = to_address
+        self.from_address = from_address
+        self.cost = cost
+        self.track = track
+
+    def __str__(self):
+        return (f'Отправление {self.track} из {self.from_address} в {self.to_address}. '
+               f'Стоимость: {self.cost} рублей')
---
+++ lesson_03/smartphone.py
@@ -0,0 +1,6 @@
+class Smartphone:
+
+    def __init__(self, brand, model, number):
+        self.br = brand
+        self.mo = model
+        self.nu = number
